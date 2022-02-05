dc95ca59a43104b58e11fec39b5e395d6afbbdba
Pete Seeling <peteseeling@gmail.com>
2022-02-04 18:57:32 -0600

    keep working on filter function

--- src/App.js
+++ src/App.js
@@ -52,15 +52,13 @@ function App() {
 
   function handleFilterGoblins(search) {
     // use the filter method to get an array of goblins whose name includes this search argument
-    if (search) {
-      const filteredGoblins = goblins.filter(goblin => goblin.name.includes(search));
+    
+    const filteredGoblins = goblins.filter(goblin => goblin.name.includes(search));
 
-      search ? setFilteredGoblins(filteredGoblins) : setFilteredGoblins(filteredGoblins);
-    }
-    else {
-      setFilteredGoblins(goblins);
-    }
+    search ? setFilteredGoblins(filteredGoblins) : setFilteredGoblins(goblins);
   }
+    
+  
     // if there is a search argument, set the filtered goblins to the filtered goblins
     // if the search argument is undefined, set the filtered goblins in state to just be the array of all goblins
 
